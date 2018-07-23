72820c7c09b67942500e1afaa4174feb07403f48
xunxingyuan <xunxingyuan@gmail.com>
2018-07-23 22:39:52 +0800

    change backend

--- src/views/users.vue
+++ src/views/users.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="users">
+        <header>
+            <p class="logo">U Know</p>
+            <div class="nav">
+                <span>主页</span>
+                <span>{{user}}</span>
+            </div>
+        </header>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
   export default {
-    name: "users"
+    name: "users",
+    data(){
+      return {
+        user: ''
+      }
+    },
+    mounted(){
+      this.user = window.sessionStorage.getItem('user')
+    }
   }
 </script>
 
@@ -14,5 +29,23 @@
   .users{
       height: 100%;
       width: 100%;
+      header{
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          height: 10%;
+          padding: 0 8%;
+          background: #f9f9f9;
+          .logo{
+              font-size: 2rem;
+              margin: 0;
+          }
+          .nav{
+              font-size: 1.1rem;
+              span{
+                  margin-left: 1rem;
+              }
+          }
+      }
   }
 </style>
--- src/views/users/info.vue
+++ src/views/users/info.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="info">
-    <p>用户名：{{userInfo.name}}</p>
-    <p>昵称：{{userInfo.nick}}</p>
-    <p>邮箱：{{userInfo.email}}</p>
-    <p>手机号码：{{userInfo.phone}}</p>
-    <p>加入时间：{{new Date(userInfo.create).toLocaleDateString()}}</p>
+    <div class="left">
+      <p class="active">用户信息</p>
+      <p>我的消息</p>
+      <p>我的关注</p>
+      <p>我的发布</p>
+      <p>我的钱包</p>
+    </div>
+    <div class="right">
+      <p>用户名：{{userInfo.name}}</p>
+      <p>昵称：{{userInfo.nick}}</p>
+      <p>邮箱：{{userInfo.email}}</p>
+      <p>手机号码：{{userInfo.phone}}</p>
+      <p>加入时间：{{new Date(userInfo.create).toLocaleDateString()}}</p>
+    </div>
+
   </div>
 </template>
 
@@ -29,6 +39,7 @@
       }).then((res)=>{
         if(res&&res.data.code === 200){
           this.userInfo = res.data.data
+          window.sessionStorage.setItem('user',this.userInfo.name)
         }
       })
     }
@@ -37,6 +48,25 @@
 
 <style lang='less'>
   .info{
-  
+    height: 90%;
+    display: flex;
+    background: #fff;
+    .left{
+      min-width: 15rem;
+      padding-top: 2rem;
+      p{
+        font-size: 0.9rem;
+        line-height: 2;
+        cursor: pointer;
+      }
+      .active{
+        color: #2c98e4;
+      }
+    }
+    .right{
+      flex: 1;
+      border-left: solid 1px #ddd;
+      min-height: 100%;
+    }
   }
 </style>
